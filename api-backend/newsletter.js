873f859c2407ca0709ccfef0f27f5ee7c4b8d908
BrunoCellenza <cellenza.bruno@hotmail.com>
2025-10-18 12:41:44 -0300

    updates

--- api-backend/newsletter.js
+++ api-backend/newsletter.js
@@ -10,8 +10,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+res.setHeader('Access-Control-Max-Age', '86400');
+
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
